71d72ab822f310b62598b42ef71f1aa7893d2e2a
antonioitsoft <antonio.marvizon@itsoft.es>
2022-06-06 16:29:46 +0200

    Tabla datos accesibilidad
    Tabla datos accesibilidad

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,5 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Visor_Aragon_Angular');
   });
 
- 
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Visor_Aragon_Angular app is running!');
+  });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,49 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainViewComponent } from './main-view/main-view.component';
+import { VisorComponent } from './visor/visor.component';
+import { TablaAccesibilidadComponent } from './tabla-accesibilidad/tabla-accesibilidad.component';
+
 import {InputTextModule} from 'primeng/inputtext';
 import {DropdownModule} from 'primeng/dropdown';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ButtonModule} from 'primeng/button';
-import { AppconfigService } from './services/appconfig.service';
-import { HttpClientModule  } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSortModule} from '@angular/material/sort';
+import {MultiSelectModule} from 'primeng/multiselect';
+//import { MatPaginatorIntl } from '@angular/material/paginator';
+import {PaginatorModule} from 'primeng/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent
+    TablaAccesibilidadComponent,
+    VisorComponent
   ],
   imports: [
+    MatProgressSpinnerModule,
+    PaginatorModule,
+    MatTableModule,
+    MultiSelectModule,
     BrowserModule,
     AppRoutingModule,
     InputTextModule,
     FormsModule,
     DropdownModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     ButtonModule,
-    HttpClientModule
+    MatIconModule,
+    HttpClientModule,
+    //MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [AppconfigService],
+  //providers: [{ provide: MatPaginatorIntl, useValue: CustomPaginator() }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
